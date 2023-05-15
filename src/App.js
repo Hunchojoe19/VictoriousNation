c403f81da6deb6e408acae39c90965e204785f9e
Joseph Enubiak <enubiakjoseph@gmail.com>
2023-05-15 21:47:57 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -1,14 +1,4 @@
 import "./App.css";
-import Landing from "./components/landing_page/Landing";
-import { Routes, Route } from "react-router-dom";
-import SecondPage from "./components/second_page/SecondPage";
-import Picture from "./components/landing_page/Picture";
-import GivePage from "./components/give_page/GivePage";
-
-function App() {
-  return (
-    <Routes>
-      <Route>
         <Route path="/" element={<Landing />} />
         <Route path="/campuses" element={<SecondPage />} />
         <Route path="/about_vcn" element={<Picture />} />
